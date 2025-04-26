e6b404f87b89d54f291351775db04a034be1a061
Moeen Ahmed <moeen.ahmed@ymail.com>
2025-04-26 15:33:05 +0800

    clones from acme-accounting original repo, alternatively it can be set as upstream but for sake of simplicity clone it simply.

---
+++ db/models/User.ts
@@ -0,0 +1,35 @@
+import {
+  Table,
+  Column,
+  Model,
+  BelongsTo,
+  ForeignKey,
+  PrimaryKey,
+  AutoIncrement,
+} from 'sequelize-typescript';
+import { Company } from './Company';
+
+export enum UserRole {
+  accountant = 'accountant',
+  corporateSecretary = 'corporateSecretary',
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model {
+  @AutoIncrement
+  @PrimaryKey
+  @Column
+  declare id: number;
+
+  @Column
+  declare name: string;
+
+  @Column
+  declare role: UserRole;
+
+  @ForeignKey(() => Company)
+  declare companyId: number;
+
+  @BelongsTo(() => Company)
+  company: Company;
+}
